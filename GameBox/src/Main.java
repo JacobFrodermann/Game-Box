90a429e3c19b873008c227c25161bc42fc6a6e25
Red-S2 <66247321+Red-S2@users.noreply.github.com>
2021-11-04 21:04:24 +0100

    Der Code

---
+++ GameBox/src/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+class Main {
+    public static void main(String[] args) {
+        Main Main = new Main();
+        Main.Main();
+    }
+    void Main() {
+        JFrame frame = new JFrame("Menu");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1001, 600);
+        frame.setVisible(true);
+    }
+}
