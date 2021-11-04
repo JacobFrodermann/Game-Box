6e2e431693ffe7eff4d0046343c8d17c38e45654
247-coder (Vincent Westphal) <vincent-wst@mail.cloud.exedatpro.de>
2021-11-04 21:13:51 +0100

    Canvas preparation

--- GameBox/src/Main.java
+++ GameBox/src/Main.java
@@ -1,14 +1,55 @@
+import java.awt.Canvas;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+
 import javax.swing.JFrame;
 
 class Main {
-    public static void main(String[] args) {
-        Main Main = new Main();
-        Main.Main();
-    }
-    void Main() {
-        JFrame frame = new JFrame("Menu");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(1001, 600);
-        frame.setVisible(true);
-    }
+	public JFrame frame;
+	public Canvas canvas;
+
+	public static void main(String[] args) {
+		Main main = new Main();
+		main.init();
+		while(true) {
+			render(main.canvas, main.draw(main.canvas.getSize()));
+		}
+	}
+
+	public void init() {
+		frame = new JFrame();
+		frame.setTitle("GameBox");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		canvas = new Canvas();
+		frame.add(canvas);
+		canvas.setPreferredSize(new Dimension(800, 600));
+		frame.pack();
+		frame.setVisible(true);
+		canvas.createBufferStrategy(2);
+	}
+
+	public BufferedImage draw(Dimension size) {
+		BufferedImage result = new BufferedImage(size.width, size.height, BufferedImage.TYPE_INT_RGB);
+		Graphics2D g = (Graphics2D) result.getGraphics();
+		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+		
+		// Rendering
+		g.drawString("hi jacob", size.width / 2, size.height / 2);
+
+		return result;
+	}
+
+	public static void render(Canvas canvas, BufferedImage img) {
+		BufferStrategy bs = canvas.getBufferStrategy();
+		Graphics g = bs.getDrawGraphics();
+		g.drawImage(img, 0, 0, img.getWidth(), img.getHeight(), null);
+		g.dispose();
+		bs.show();
+	}
 }
