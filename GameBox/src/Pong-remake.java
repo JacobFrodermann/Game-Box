266abcc773fc312393667ae3e061f11f0ff0bc0c
Red-S2 <66247321+Red-S2@users.noreply.github.com>
2021-11-04 22:41:18 +0100

    Update

---
+++ GameBox/src/Pong-remake.java
@@ -0,0 +1,29 @@
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+
+
+public class Pong_remake {
+
+    int Wall1pos;
+    int Wall2pos;
+    Dimension Ballpos;
+
+    void Innit() {
+        Wall1pos = 100;
+        Wall2pos = 100;
+        Ballpos = new Dimension(180,280);
+    }
+
+    void loop() {
+        if () {
+            
+        }
+    }
+    
+}
