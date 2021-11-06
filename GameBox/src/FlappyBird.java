11acc3b47981ed95589e7e710e4cc5789c5c7368
247-coder (Vincent Westphal) <vincent-wst@mail.cloud.exedatpro.de>
2021-11-06 23:31:50 +0100

    Fix Garbage Code

--- GameBox/src/FlappyBird.java
+++ GameBox/src/FlappyBird.java
@@ -1,52 +1,42 @@
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOError;
 import java.io.IOException;
-import java.nio.Buffer;
-import java.sql.Array;
-import java.awt.Color;
-import java.awt.*;
 
 import javax.imageio.ImageIO;
 
 public class FlappyBird implements Game {
-	Array pipes[];
 	BufferedImage bird;
 	BufferedImage section;
 	BufferedImage end;
-	Color Sky;
+	Color sky = new Color(52, 174, 235);
 
-	Double VelY;
-	Double BirdY;
-	int Score;
-	Boolean alive;
+	Double VelY = 0.0;
+	Double BirdY = 300.0;
+	int Score = 0;
 
-	public BufferedImage draw(Dimension size) throws IOException{
-
-		VelY = 0.0;
-		BirdY = 300.0;
-		Score = 0;
-		alive = true;
-
-		Sky = new Color(52, 174, 235);
+	public FlappyBird() throws IOException {
 		bird = ImageIO.read(new File("The Bird.png"));
-		section = ImageIO.read(new File("PipeSegment"));
-		end = ImageIO.read(new File("PipeEnd"));
+		section = ImageIO.read(new File("PipeSegment.png"));
+		end = ImageIO.read(new File("PipeEnd.png"));
+	}
+
+	public BufferedImage draw(Dimension size) {
 		BufferedImage result = new BufferedImage(size.width, size.height, BufferedImage.TYPE_INT_RGB);
 		Graphics2D g = (Graphics2D) result.getGraphics();
 		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
 		
 		// Rendering
-		g.setColor(Sky);;
-		g.fill(new Rectangle(new Point(),size));
-		while alive {
-			g.drawImage(bird, 20, BirdY, 40, 40, Sky, null);
+		g.setColor(sky);
+		g.fill(new Rectangle(new Point(), size));
+		g.drawImage(bird, 20, (int) Math.round(BirdY), 40, 40, null);
 
-		}
 		return result;
 	}
 }
