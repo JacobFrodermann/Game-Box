4ab736c524305aacf546058a4c87c0aa50fa0b54
Red-S2 <66247321+Red-S2@users.noreply.github.com>
2021-11-07 01:22:09 +0100

    Flapp

--- GameBox/src/FlappyBird.java
+++ GameBox/src/FlappyBird.java
@@ -10,21 +10,25 @@ import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import java.util.Random;
 
 public class FlappyBird implements Game {
 	BufferedImage bird;
-	BufferedImage section;
-	BufferedImage end;
+	BufferedImage Pipe;
+	BufferedImage dead;
 	Color sky = new Color(52, 174, 235);
 
 	Double VelY = 0.0;
 	Double BirdY = 300.0;
 	int Score = 0;
+	int pipe1Y; 
+	int i = 0;
 
 	public FlappyBird() throws IOException {
 		bird = ImageIO.read(new File("The Bird.png"));
-		section = ImageIO.read(new File("PipeSegment.png"));
-		end = ImageIO.read(new File("PipeEnd.png"));
+		dead = ImageIO.read(new File("Dead.png"));
+		Pipe = ImageIO.read(new File("Pipe.png"));
+		pipe1Y = new Random().nextInt(500);
 	}
 
 	public BufferedImage draw(Dimension size) {
@@ -36,12 +40,32 @@ public class FlappyBird implements Game {
 		// Rendering
 		g.setColor(sky);
 		g.fill(new Rectangle(new Point(), size));
-		g.drawImage(bird, 20, (int) Math.round(BirdY), 40, 40, null);
 
+
+		if (BirdY > 600 || BirdY < -20 || (BirdY < pipe1Y  || BirdY > pipe1Y-100 && (Score > 80+i*2 && Score < 160+i*2))) {
+			g.setColor(Color.BLACK);
+			g.drawImage(Pipe, (100-Score/2)+i, -600 + pipe1Y, null);
+			g.drawImage(dead, 50, 250, null);
+		} else {
+			g.drawImage(Pipe, (100-Score/2)+i, -600 + pipe1Y, null);
+			g.drawImage(bird, 20, (int) Math.round(BirdY), 40, 40, null);
+			BirdY += VelY;
+			VelY += 0.1;
+			Score += 1;
+		}
+		if ((100-Score/2)+i < -50) {
+			i += 450;
+			pipe1Y = new Random().nextInt(500);
+		}
 		return result;
+
+
 	}
 
 	public void keyPressed(KeyEvent event) {
+		if (event.getKeyCode() == KeyEvent.VK_SPACE) {
+			VelY -= 5;
+		}
 	}
 	public void keyReleased(KeyEvent event) {
 	}
